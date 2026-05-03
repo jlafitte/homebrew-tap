c00937299fcb853e7c3437515d196a3aec16df3a
John Lafitte <john.lafitte@gmail.com>
2026-05-03 14:13:09 -0500

    Add GeminiWatermarkTool v0.2.8

---
+++ gemini-watermark-tool.rb
@@ -0,0 +1,15 @@
+class GeminiWatermarkTool < Formula
+  desc "Tool to remove watermarks using reverse alpha blending"
+  homepage "https://github.com/allenk/GeminiWatermarkTool"
+  url "https://github.com/allenk/GeminiWatermarkTool/releases/download/v0.2.8/GeminiWatermarkTool-macOS-Universal.zip"
+  sha256 "dc693e64680886361cf5a20ccef2947d1d04004df815cd21944f38813b819e3a"
+  license "MIT"
+
+  def install
+    bin.install "GeminiWatermarkTool"
+  end
+
+  test do
+    system "#{bin}/GeminiWatermarkTool", "--version"
+  end
+end
