eae57daf5e4cb3e368b9af9a45514122ba303ac3
John Lafitte <john.lafitte@gmail.com>
2026-05-07 13:40:24 -0500

    feat: Add auto-update workflow and PixelPerfect cask
    This commit introduces a GitHub Actions workflow to automatically update
    the PixelPerfect cask. It also adds the PixelPerfect cask definition
    itself, enabling it to be managed by Homebrew.

---
+++ Casks/pixelperfect.rb
@@ -0,0 +1,16 @@
+cask "pixelperfect" do
+  version "1.5"
+  sha256 "94c2fbb35850b3703e1677959421c761b4ad9860a8d17f383c07900fbd89c99a"
+
+  url "https://github.com/cormiertyshawn895/PixelPerfect/releases/download/#{version}/PixelPerfect.#{version}.zip"
+  name "Pixel Perfect"
+  desc "Increase the text size of iPhone and iPad apps on Mac"
+  homepage "https://github.com/cormiertyshawn895/PixelPerfect"
+
+  app "Pixel Perfect.app"
+
+  zap trash: [
+    "~/Library/Application Support/PixelPerfect",
+    "~/Library/Preferences/com.cormiertyshawn895.PixelPerfect.plist",
+  ]
+end
