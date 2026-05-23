56c71c62c85541cab101daf3aec33fc20efece04
John Lafitte <jlafitte@users.noreply.github.com>
2026-05-23 12:29:37 +0000

    gemini-watermark-tool 0.3.1

--- Formula/gemini-watermark-tool.rb
+++ Formula/gemini-watermark-tool.rb
@@ -1,8 +1,8 @@
 class GeminiWatermarkTool < Formula
   desc "Tool to remove watermarks using reverse alpha blending"
   homepage "https://github.com/allenk/GeminiWatermarkTool"
-  url "https://github.com/allenk/GeminiWatermarkTool/releases/download/v0.3.0/GeminiWatermarkTool-macOS-Universal.zip"
-  sha256 "8740fe924aeef5bf63acbd665334914a4eac61dc5aaf9bff6fc68c9c3a4793f9"
+  url "https://github.com/allenk/GeminiWatermarkTool/releases/download/v0.3.1/GeminiWatermarkTool-macOS-Universal.zip"
+  sha256 "5d425d49d74c74449a9d5fe267b2587ae036c7931b66c5a9c13db568a72f45d8"
   license "MIT"
 
   livecheck do
